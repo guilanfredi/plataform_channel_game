bcff759d91f7a02f3a5b7b09048fd50989307818
guilanfredi <gui.lanfredi@gmail.com>
2022-09-12 22:03:36 -0300

    first commit

---
+++ lib/constants/styles.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+TextStyle TextStyle36 = const TextStyle(fontSize: 36, color: Colors.white);
+TextStyle TextStyle75 = const TextStyle(fontSize: 75, color: Colors.white);
